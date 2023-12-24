98076e6702182b2a1e09500ccb7f1b1756d181f9
fajarhidayatt <fadjhar10@gmail.com>
2023-12-24 09:05:00 +0700

    create simple dashboard

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,27 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Dashboard</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+</head>
+
+<body>
+
+    <div class="d-flex flex-column align-items-center position-absolute top-50 start-50 translate-middle">
+        {{-- Tampilkan nama pengguna yang sedang login --}}
+        <h1 class="">Selamat Datang, {{ Auth::user()->name }}</h1> 
+
+        {{-- Logout --}}
+        <form action="/logout" method="post">
+            @csrf
+            <button type="submit" class="btn btn-danger">Logout</button>
+        </form>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+</body>
+
+</html>
